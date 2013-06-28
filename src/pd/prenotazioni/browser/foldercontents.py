516d7faad79e9ac189f3057dcae0e7cc40b62f95
ale-rt <alessandro.pisa@redturtle.it>
2013-06-28 14:16:54 +0000

    Removing unwanted stuff

---
+++ src/pd/prenotazioni/browser/foldercontents.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from Acquisition import aq_inner
+from plone.app.content.browser.foldercontents import (FolderContentsTable
+                                                      , FolderContentsBrowserView
+                                                      , FolderContentsView)
+
+
+class PrenotazioniFolderContentsTable(FolderContentsTable):
+    """
+    The foldercontents table WITH NO BUTTONS
+    """
+    buttons = []
+
+
+class PrenotazioniFolderContentsView(FolderContentsView):
+    '''
+    The foldercontents CUSTOMIZED
+    '''
+    def contents_table(self):
+        table = PrenotazioniFolderContentsTable(aq_inner(self.context), self.request)
+        return table.render()
+
+
+class PrenotazioniFolderContentsBrowserView(FolderContentsBrowserView):
+    table = PrenotazioniFolderContentsTable
