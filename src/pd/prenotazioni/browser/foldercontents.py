c84bb28280ebfd8f3746508bed6fb40ee95fa662
nicolasenno <nicola.senno@redturtle.it>
2013-11-14 13:58:38 +0000

    show folder action if the user is a Manager

--- src/pd/prenotazioni/browser/foldercontents.py
+++ src/pd/prenotazioni/browser/foldercontents.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_inner
+from plone import api
 from plone.app.content.browser.foldercontents import (FolderContentsTable
                                                       , FolderContentsBrowserView
                                                       , FolderContentsView)
@@ -17,8 +18,15 @@ class PrenotazioniFolderContentsView(FolderContentsView):
     The foldercontents CUSTOMIZED
     '''
     def contents_table(self):
-        table = PrenotazioniFolderContentsTable(aq_inner(self.context), self.request)
-        return table.render()
+        ''' Hide folfer action if the user doesn't have Manager role
+        '''
+        roles = api.user.get_roles()
+        if 'Manager' in roles:
+            table = FolderContentsTable(aq_inner(self.context), self.request)
+            return table.render()
+        else:
+            table = PrenotazioniFolderContentsTable(aq_inner(self.context), self.request)
+            return table.render()
 
 
 class PrenotazioniFolderContentsBrowserView(FolderContentsBrowserView):
