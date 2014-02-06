e986c2fe2c510203787de7a106735a3245ecfa4f
ale-rt <alessandro.pisa@redturtle.it>
2014-02-06 12:00:50 +0000

    label personalizzate

--- src/pd/prenotazioni/browser/prenotazione_print.py
+++ src/pd/prenotazioni/browser/prenotazione_print.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from plone.memoize.view import memoize
-from rg.prenotazioni.browser.prenotazione_print import PrenotazionePrint as BasePrintView
+from rg.prenotazioni.browser.prenotazione_print import PrenotazionePrint as BasePrintView  # noqa
 
 
 class PrenotazionePrint(BasePrintView):
     '''
     This is a view to proxy autorizzazione
     '''
+    description = u'Attendi conferma via mail o SMS'
 
     @property
     @memoize
@@ -14,5 +15,4 @@ class PrenotazionePrint(BasePrintView):
         """
         Link to prenotazioni_print_pdf
         """
-        return self.context.absolute_url() + '/prenotazione_print_pdf?uid='+ self.request.get("uid")
-
+        return self.context.absolute_url() + '/prenotazione_print_pdf?uid=' + self.request.get("uid")  # noqa
