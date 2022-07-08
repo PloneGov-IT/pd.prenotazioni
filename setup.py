9f8fb02acec1ccf33ac7a503478fab90365ced3e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-07-08 14:15:55 +0200

    Back to development: 1.6.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "1.6.0"
+version = "1.6.1.dev0"
 
 long_description = (
     read("README.rst") + "\n\n" + "Change history\n"
